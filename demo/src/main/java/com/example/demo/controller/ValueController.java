ca4d47b30ed673cde4924846c37e15ffd10eee36
Douglas Fernandes <douglasfpdasilvajunior@gmail.com>
2019-10-18 09:50:39 -0300

    ajustar

--- demo/src/main/java/com/example/demo/controller/ValueController.java
+++ demo/src/main/java/com/example/demo/controller/ValueController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,17 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/values")
 @Api(tags = { " Values " }, value = "Api for return values")
-@Slf4j
 public class ValueController {
 
-    @ApiResponses(value = { @ApiResponse(code = 200, message = "Return a list with values"),
-            @ApiResponse(code = 404, message = "Values not found"), })
-    @GetMapping()
+    @ApiResponses(
+        value = { //
+            @ApiResponse(code = 200, message = "Return a list with values"), //
+            @ApiResponse(code = 404, message = "Values not found"), //
+    })
+    @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<List<String>> listAll() {
         List<String> list = new ArrayList<String>();
 
