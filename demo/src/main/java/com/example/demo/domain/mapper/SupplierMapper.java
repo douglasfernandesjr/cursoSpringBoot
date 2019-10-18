76e7b30942bb27597f5984902763178218827d2b
Douglas Fernandes <douglasfpdasilvajunior@gmail.com>
2019-10-18 16:42:26 -0300

    atualizar

--- demo/src/main/java/com/example/demo/domain/mapper/SupplierMapper.java
+++ demo/src/main/java/com/example/demo/domain/mapper/SupplierMapper.java
@@ -3,17 +3,17 @@ package com.example.demo.domain.mapper;
 import com.example.demo.domain.dto.response.SupplierListResponse;
 import com.example.demo.domain.entities.Supplier;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SupplierMapper {
 
+    @Autowired
+    ModelMapper mapper;
+
     public SupplierListResponse toDto(Supplier input) {
-        SupplierListResponse item = new SupplierListResponse();
-        item.setCity(input.getCity());
-        item.setCompanyName(input.getCompanyName());
-        item.setCountry(input.getCountry());
-        item.setId(input.getId());
-        return item;
+        return  mapper.map(input, SupplierListResponse.class);
     }
 }
